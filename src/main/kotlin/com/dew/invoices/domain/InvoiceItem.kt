8c3330f6b75870ae1f7970e283a22169fd68bb4c
Carlos Pavajeau <cantte098@gmail.com>
2022-04-06 18:16:19 -0500

    refactor: quantity instead of amount

--- src/main/kotlin/com/dew/invoices/domain/InvoiceItem.kt
+++ src/main/kotlin/com/dew/invoices/domain/InvoiceItem.kt
@@ -8,12 +8,12 @@ import javax.validation.constraints.NotBlank
 data class InvoiceItem(
     @field:BsonProperty("product") @param:BsonProperty("product") @field:NotBlank val product: Product,
     @field:BsonProperty("price") @param:BsonProperty("price") @field:NotBlank val price: Float,
-    @field:BsonProperty("amount") @param:BsonProperty("amount") @field:NotBlank val amount: Int,
+    @field:BsonProperty("quantity") @param:BsonProperty("quantity") @field:NotBlank val quantity: Int,
     @field:BsonProperty("tax") @param:BsonProperty("tax") @field:NotBlank val tax: Float,
     @field:BsonProperty("discount") @param:BsonProperty("discount") @field:NotBlank val discount: Float,
 ) {
     @field:BsonProperty("subTotal")
-    val subTotal: Float = price * amount
+    val subTotal: Float = price * quantity
 
     @field:BsonProperty("total")
     val total: Float = (subTotal * (1 + tax)) * (1 - discount)
